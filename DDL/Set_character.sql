9b44dd3ebc52a962efa2f46cd39475060760c194
KevkevCon <kevin@consulytic.com>
2024-07-05 10:27:54 -0300

    Created character table

---
+++ DDL/Set_character.sql
@@ -0,0 +1,51 @@
+DO $$
+BEGIN
+
+    -- Begin a transaction
+
+    BEGIN
+ 
+
+            -- Your SQL script logic here
+
+            -- Example: Creating a table
+
+          CREATE TABLE IF NOT EXISTS raw."Character"
+          (
+                FirstName Character varying,
+                LastName Character varying,
+                D.O.B Date,
+                Gender Character varying,
+                Shool Character varying,
+                Group Integer,
+                Sensei Character varying,
+                Missions_completed integer,
+                'Level' integer,
+                'Atribute 1' Character varying,
+
+            
+          );
+
+
+        -- Commit the transaction
+
+ 
+ 
+
+    EXCEPTION
+
+        -- Handle any errors that occur
+
+        WHEN OTHERS THEN
+
+            -- Rollback the transaction
+
+            ROLLBACK;
+
+            -- Rethrow the error
+
+            RAISE;
+
+    END;
+commit;
+END$$;
