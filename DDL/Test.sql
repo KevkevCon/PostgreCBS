7b76acd44b41a2824fd661f7ccf49785e52cfa7d
KevkevCon <kevin@consulytic.com>
2024-08-07 15:01:02 -0300

    added test script

---
+++ DDL/Test.sql
@@ -0,0 +1,46 @@
+DO $$
+BEGIN
+ 
+    -- Begin a transaction
+ 
+    BEGIN
+
+ 
+            -- Your SQL script logic here
+ 
+            -- Example: Creating a table
+ 
+          CREATE TABLE IF NOT EXISTS raw."Test"
+          (
+                FirstName Character varying,
+                Test Character varying,
+                tester Character varying,
+                tested boolean
+
+ 
+            
+          );
+ 
+ 
+        -- Commit the transaction
+ 
+
+
+ 
+    EXCEPTION
+ 
+        -- Handle any errors that occur
+ 
+        WHEN OTHERS THEN
+ 
+            -- Rollback the transaction
+ 
+            ROLLBACK;
+ 
+            -- Rethrow the error
+ 
+            RAISE;
+ 
+    END;
+commit;
+END$$;
